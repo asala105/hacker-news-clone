9614b5365aa9b08542a2da0e1bb5a15e801cb053
Asala El Ghandour <asala@swftbox.com>
2022-10-30 12:52:44 +0200

    footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-const NavLinkStyle = {
-  textDecoration: 'none',
-  color: '#000000',
-};
+
 export default function Footer() {
   return (
     <div
@@ -10,41 +7,50 @@ export default function Footer() {
         display: 'flex',
         justifyContent: 'center',
         alignContent: 'center',
+        textAlign: 'center',
       }}>
       <div>
-        <div>Applications are open for YC Winter 2023</div>
         <div>
-          <a href='#' style={{ ...NavLinkStyle }}>
-            Guidelines
+          <a
+            className='article-header'
+            href='https://www.ycombinator.com/apply/'>
+            Applications are open for YC Winter 2023
           </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            FAQ
-          </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            Lists
-          </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            API
-          </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            Security
-          </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            Legal
-          </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            Apply to YC
-          </a>
-          |
-          <a href='#' style={NavLinkStyle}>
-            Contact
-          </a>
+        </div>
+        <div>
+          <p className='footer-links-wrapper '>
+            <a href='#' className='footer-links'>
+              Guidelines
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              FAQ
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              Lists
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              API
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              Security
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              Legal
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              Apply to YC
+            </a>
+            |
+            <a href='#' className='footer-links'>
+              Contact
+            </a>
+          </p>
         </div>
       </div>
     </div>
